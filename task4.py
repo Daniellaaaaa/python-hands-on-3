384db18aca1d01426fbd61e148cd69f0bffb0176
Daniellaaaaa <onyinyedanielladuru@gmail.com>
2025-07-24 16:48:57 +0100

    Question 4

--- task4.py
+++ task4.py
@@ -5,8 +5,26 @@
     1. The student realizes they need to add "ENG 101" at the beginning of their course list 
        since English is a prerequisite for other courses.
     2. The Academic Office informs them that "GST 101" has been moved to next semester, 
+
        so it should be removed from the current list.
     3. The student wants to add "BIO 101" right after "CHM 101" to complete their science requirements.
     4. Find and display the total number of courses the student is now registered for.
     5. The student wants to know what their 3rd course in the list is.
 """
+
+# Question 1
+
+courses = ["MTH 101", "PHY 101", "CHM 101", "CSC 101", "GST 101"]
+courses.insert(0, "ENG 101")
+print("1. ", courses)
+# Question 2
+
+courses.remove(courses[-1])
+print("2. ", courses) 
+# Question 3
+courses.insert(4, "BIO 101")
+print("3. ", courses)
+# Question 4
+print("4.", len(courses))
+# Question 5
+print("5. ", courses[2])
